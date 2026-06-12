a51d903dee1477ed9516b0ac9cfc6a9f78d7a38a
D4F474 <dimitarvveselinov@gmail.com>
2026-06-12 16:55:54 +0300

    change url

--- frontend/frontend/src/app/interceptor/api-url-interceptor.ts
+++ frontend/frontend/src/app/interceptor/api-url-interceptor.ts
@@ -12,7 +12,7 @@ export const apiUrlInterceptor: HttpInterceptorFn = (req, next) => {
   }
 
   const apiReq = req.clone({
-    url: `${LOCAL_API_URL}${req.url.startsWith('/') ? req.url : '/' + req.url}`,
+    url: `${API_URL}${req.url.startsWith('/') ? req.url : '/' + req.url}`,
   });
 
   return next(apiReq);
